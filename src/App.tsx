41d737a22a1f3c58e59f92b511c6676659ef858b
jvillegasl <jvillegasl@uni.pe>
2023-04-11 00:41:56 -0500

    feat: scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.scss";
 
+import selfPic from "@/assets/images/pics/self.png";
+import { useMemo } from "react";
+import { CommentsSection } from "./components";
+import { User } from "./types";
+import { UserContext } from "./context";
+
 function App() {
-    return <div className="l-app"></div>;
+    const user = useMemo<User>(
+        () => ({
+            channelUrl: "",
+            displayName: "jvillegasl2",
+            profileImageUrl: selfPic,
+            channelId: "1234567890",
+        }),
+        []
+    );
+
+    return (
+        <UserContext.Provider value={user}>
+            <div className="c-app">
+                <CommentsSection />
+            </div>
+        </UserContext.Provider>
+    );
 }
 
 export default App;
